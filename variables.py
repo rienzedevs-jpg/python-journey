96c51f419d30a0fcd8707a15e29f9be8941925c7
Rienze <rienze.devs@gmail.com>
2026-04-29 15:57:44 +0800

    fixed git setup, added existing files

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Rienze"
+age = 28
+is_developer = True
+height = 5.11
+
+print(name)
+print(age)
+print(is_developer)
+print(height)
+print(age + 2)
+
+
+print(type(name))
+print(type(age))
+print(type(is_developer))
+print(type(height))
