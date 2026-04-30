b9265d4dc6678dd9d5da3920bf523f99f6eaffbf
Rienze <rienze.devs@gmail.com>
2026-04-30 20:35:20 +0800

    learned user input and type casting

---
+++ input.py
@@ -0,0 +1,12 @@
+# A simple interactive program
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+language = input("What programming language are you learning? ")
+
+print(f"\nHello {name}!")
+print(f"You are {age} years old.")
+print(f"You are learning {language}.")
+print(f"In 5 years you will be {age + 5} years old and probably an expert at {language}!")
+
+
